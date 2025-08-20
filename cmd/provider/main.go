82c0dd97101cad988f87158f79314aafa9c03ee9
Alexander Chernov <alexander.chernov@globallogic.com>
2025-08-20 22:19:57 +0100

    typo

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -58,7 +58,7 @@ func main() {
 	ctrl.SetLogger(zap.New(zap.WriteTo(io.Discard)))
 
 	zl := zap.New(zap.UseDevMode(*debug))
-	logger := logging.NewLogrLogger(zl.WithName("provider-palette"))
+	logger := logging.NewLogrLogger(zl.WithName("provider-minio"))
 	if *debug {
 		// The controller-runtime runs with a no-op logger by default. It is
 		// *very* verbose even at info level, so we only provide it a real
